50806c645d646958652db7ec54bc4a171a8434b2
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-07 17:45:53 +0600

    fix: proxy done

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 const adminOnlyRoutes = [
-  "/", // ✅ dashboard
+  "/",
   "/businesse-management",
   "/commission-plan",
   "/payment-history",
@@ -19,9 +19,8 @@ export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const isGuest = !token;
-  const userRole = token?.role?.toUpperCase();
+  const userRole = token?.role?.toLowerCase();
 
-  // 🔥 Special handling for "/"
   const isDashboard = pathname === "/";
 
   const isProtectedRoute =
@@ -30,17 +29,14 @@ export async function proxy(request: NextRequest) {
       (route) => route !== "/" && pathname.startsWith(route),
     );
 
-  // 1️⃣ Guest block
   if (isGuest && isProtectedRoute) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // 2️⃣ Non-admin block
-  if (!isGuest && isProtectedRoute && userRole !== "ADMIN") {
-    return NextResponse.redirect(new URL("/", request.url));
+  if (!isGuest && isProtectedRoute && userRole !== "admin") {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // 3️⃣ Prevent logged-in user from auth pages
   const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
 
   if (!isGuest && isAuthRoute) {
